6489f8560ffee6c73c59aaf92f2ed6879a57e26c
miguel angel <mikeangelvl1097@gmail.com>
2021-08-25 14:55:55 -0500

    primer deploy

---
+++ main.js
@@ -0,0 +1,52 @@
+
+let rombo=document.getElementById("rombo");
+let timer= document.getElementById("cont");
+
+let cont=1;
+let timerForward=0;
+
+setInterval(()=>{
+    cont++;
+
+    console.log(cont)
+    rombo.style=`transform: rotate(${cont}deg);`;
+
+
+},1000);
+
+
+setInterval(()=>{
+	timerForward++;
+	cont.innerHTML=`${timerForward}`
+},2000);
+
+
+const data=[
+    {
+        "name":"miki",
+        "age":22
+    },
+    {
+        "name":"andrea",
+        "age":29
+    },
+    {
+        "name":"sebastian",
+        "age":78
+    },
+    {
+        "name":"anthony",
+        "age":24
+    }
+];
+let divData=document.getElementById("data");
+let input='';
+
+data.map((datos)=>{
+    input+=`
+    <li>${datos.name}</li>
+    <li>${datos.age}</li>
+    `;
+});
+
+divData.innerHTML=input;
